1a75804fbc1771d85dc60c78fe8170e7d677f3df
Benjamin Benge <bengebenjamin@gmail.com>
2025-04-23 16:49:16 +0300

    Added a admin.vue which contains a table to dynamically uodate the data. Ensured that the nav bar is black and that the item on the nav bar that is clicked becomes bolder than the others

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,14 @@ import Products from '../views/Products.vue'
 import Services from '../views/Services.vue'
 import About from '../views/About.vue'
 import Contact from '../views/Contact.vue'
+import Admin from '../views/admin.vue'
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/products', name: 'Products', component: Products },
   { path: '/services', name: 'Services', component: Services },
   { path: '/about', name: 'About', component: About },
   { path: '/contact', name: 'Contact', component: Contact },
+  { path: '/admin', name: 'Admin', component: Admin },
 ]
 const router = createRouter({
   history: createWebHistory(),
