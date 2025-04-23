c50860ef62a1900e3cafd4cee813c961d39b8ac9
Benjamin Benge <bengebenjamin@gmail.com>
2025-04-23 17:58:54 +0300

    Added Admin table and dynamic bold on thee nav bar when a page is selected

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -27,52 +27,10 @@
             </li>
           </ul>
 
-<<<<<<< HEAD
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <router-link class="nav-link" to="/products"
-                  >Products</router-link
-                >
-              </li>
-              <li class="nav-item">
-                <router-link class="nav-link" to="/services"
-                  >Services</router-link
-                >
-              </li>
-              <li class="nav-item">
-                <router-link class="nav-link" to="/about">About Us</router-link>
-              </li>
-              <li class="nav-item">
-                <router-link class="nav-link" to="/contact"
-                  >Contact Us</router-link
-                >
-              </li>
-               <li class="nav-item">
-                <router-link class="nav-link" to="/admin"
-                  >Admin</router-link
-                >
-              </li>
-            </ul>
-
-            <form class="d-flex" role="search">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-=======
           <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button class="btn btn-outline-light" type="submit">Search</button>
           </form>
->>>>>>> 1a75804fbc1771d85dc60c78fe8170e7d677f3df
         </div>
       </div>
     </nav>
